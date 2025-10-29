debb5c9a0a2771fa69724fb8fff25b4ed806fe1f
Ayush-the-uchiha <145687049+Ayush-the-uchiha@users.noreply.github.com>
2025-10-29 12:56:30 +0530

    Implement input validation for QR code generation
    Added input validation to ensure a message is entered before generating the QR code.

--- qrCodeGenerator/script.js
+++ qrCodeGenerator/script.js
@@ -1,10 +1,8 @@
-// let text = document.getElementById("inputField").value;
-// let text = document.getElementById("inputField").value;
-
 function generateQRCode() {
     let text = document.getElementById("inputField").value;
+    if(text.trim()===""){
+        alert("Please enter any message atleast....");
+    }
     let qrContainer = document.getElementById("resultContainer");
-    let Src = "hii";
-    console.log(text)
     qrContainer.src=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`;
 }
